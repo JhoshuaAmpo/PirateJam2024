314fb3b8aafa6d023de83a0f438c7e3e90da0438
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-07-19 20:15:08 -0700

    Pick up iron

---
+++ PirateJam2024/Assets/Scripts/Player/PlayerUpgrade.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUpgrade : MonoBehaviour
+{
+    private int numIronIngots;
+
+    private void Awake() {
+        numIronIngots = 0;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("IronIngot")) {
+            numIronIngots++;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
