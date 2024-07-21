2b268493467c2a17fa23ff60e83bc2d8dfd48a11
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-07-20 23:25:18 -0700

    create level costs

--- PirateJam2024/Assets/Scripts/Player/PlayerLevelUp.cs
+++ PirateJam2024/Assets/Scripts/Player/PlayerLevelUp.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerLevelUp : MonoBehaviour
 {
-    private int numIronIngots;
+    [SerializeField]
+    private List<int> nextLevelCost;
+    private int expPoints;
+    private int level = 0;
 
-    
     private void Awake() {
-        numIronIngots = 0;
+        expPoints = 0;
     }
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("IronIngot")) {
-            numIronIngots++;
+            expPoints++;
+            if (level+1 < nextLevelCost.Count && expPoints >= nextLevelCost[level]) {
+                expPoints -= nextLevelCost[level];
+                // Process level up
+                Debug.Log("Congrats you leveled up!");
+            }
             other.gameObject.SetActive(false);
         }
     }
-
-
 }
