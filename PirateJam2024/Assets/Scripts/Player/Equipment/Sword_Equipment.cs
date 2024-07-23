e07c5a5b82bfc53d04a427af9b8d1aa67432bbbc
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-07-23 14:54:17 -0700

    implement strike

--- PirateJam2024/Assets/Scripts/Player/Equipment/Sword_Equipment.cs
+++ PirateJam2024/Assets/Scripts/Player/Equipment/Sword_Equipment.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class Sword_Equipment : Equipment_Base
 {
+    List<Transform> enemiesInStrikeRange;
+    Collider strikeBox;
+
+    private void Awake() {
+        enemiesInStrikeRange = new();
+        strikeBox = GetComponent<Collider>();
+    }
     public override void ActivateObject()
     {
-        Debug.Log("Using Sword!");
+        float smallestDist = 10000000f;
+        Transform closestEnemy = null;
+        foreach(Transform enemy in enemiesInStrikeRange) {
+            float curDist = Vector3.Distance(enemy.position, strikeBox.bounds.center);
+            if (curDist < smallestDist) {
+                smallestDist = curDist;
+                closestEnemy = enemy;
+            }
+        }
+        if (closestEnemy) {
+            Debug.Log("Strinking enemy: " + closestEnemy.name);
+        }
+        // closestEnemy.TakeDamage();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        // Debug.Log(other.name + " has entered striking range");
+        enemiesInStrikeRange.Add(other.transform);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        enemiesInStrikeRange.Remove(other.transform);
     }
 }
