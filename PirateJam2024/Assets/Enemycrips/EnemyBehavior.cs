7e1f314d325b4dfa33c7e6d5ff06ecb9987de300
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-07-23 16:37:40 -0700

    add enemy ai

---
+++ PirateJam2024/Assets/Enemycrips/EnemyBehavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    Transform target;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindWithTag("Player").transform;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.enabled = true;
+        navMeshAgent.destination = target.position;
+    }
+}
