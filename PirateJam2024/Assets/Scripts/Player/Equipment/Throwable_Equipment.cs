d6a5b011951823fd9fd11aad59e651efc123bb84
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-07-25 00:30:42 -0700

    start on potion throw

--- PirateJam2024/Assets/Scripts/Player/Equipment/Throwable_Equipment.cs
+++ PirateJam2024/Assets/Scripts/Player/Equipment/Throwable_Equipment.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class Throwable_Equipment : Equipment_Base
 {
+    [SerializeField]
+    float projectileForce;
+    Rigidbody rb;
+
+    Vector3 originalPos;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        originalPos = transform.position;
+    }
+
     public override void ActivateObject()
     {
         Debug.Log("Using Throw Object!");
+        rb.useGravity = true;
+        Vector3 force = (transform.forward + transform.up).normalized * projectileForce;
+        rb.AddForce(force, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log("Succesfully hit: " + other.gameObject.name);
+        rb.velocity = Vector3.zero;
+        transform.SetPositionAndRotation(originalPos, Quaternion.identity);
+        rb.useGravity = false;
     }
 }
