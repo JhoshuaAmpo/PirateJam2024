6922e6919e98c2684485d73ce1bdb09595414898
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-07-26 23:21:02 -0700

    create new surface and nav agent type

--- PirateJam2024/Assets/Scripts/Enemycrips/EnemyBehavior.cs
+++ PirateJam2024/Assets/Scripts/Enemycrips/EnemyBehavior.cs
@@ -7,16 +7,27 @@ public class EnemyBehavior : MonoBehaviour
 {
     NavMeshAgent navMeshAgent;
     Transform target;
+    Animator animator;
     // Start is called before the first frame update
     void Awake()
     {
         navMeshAgent = transform.parent.GetComponentInChildren<NavMeshAgent>();
-        target = GameObject.FindWithTag("Player").transform;    
+        target = GameObject.FindWithTag("Player").transform;
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        HuntPlayer();
+        if (Vector3.Distance(transform.position, target.position) <= navMeshAgent.stoppingDistance) {
+            animator.SetBool("Pursue", false);
+        }
+    }
+
+    private void HuntPlayer()
+    {
+        animator.SetBool("Pursue", true);
         navMeshAgent.enabled = true;
         navMeshAgent.destination = target.position;
     }
