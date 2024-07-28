5354d26f7ef961fe3ea88cc8e26be1c861cb6a60
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-07-27 23:37:33 -0700

    update enemy behavior

--- PirateJam2024/Assets/Scripts/Enemycrips/EnemyBehavior.cs
+++ PirateJam2024/Assets/Scripts/Enemycrips/EnemyBehavior.cs
@@ -1,13 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    [SerializeField]
+    [Range(0,180)]
+    [Tooltip("Half field of view in degrees")]
+    int HalfFOV;
+
     NavMeshAgent navMeshAgent;
     Transform target;
     Animator animator;
+
+    bool isPursuing = false;
     // Start is called before the first frame update
     void Awake()
     {
@@ -19,10 +25,32 @@ public class EnemyBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HuntPlayer();
-        if (Vector3.Distance(transform.position, target.position) <= navMeshAgent.stoppingDistance) {
-            animator.SetBool("Pursue", false);
+        if (isPursuing) {
+            HuntPlayer();
         }
+        // if (Vector3.Distance(transform.position, target.position) <= navMeshAgent.stoppingDistance) {
+        //     animator.SetBool("Pursue", false);
+        // }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (!other.CompareTag("Player") || isPursuing) {return;}
+        if (PlayerInFOV()) {
+            isPursuing = true;
+        }
+    }
+
+    void Idle() {
+
+    }
+
+    // Only call in on trigger
+    private bool PlayerInFOV() {
+        Vector3 dirToPlayer = target.transform.position - transform.position;
+        dirToPlayer.y = 0;
+        float angle = Vector3.Angle(dirToPlayer, transform.forward);
+        Debug.Log("Angle diff between player and " + gameObject.name + ": " + angle);
+        return angle < HalfFOV;
     }
 
     private void HuntPlayer()
